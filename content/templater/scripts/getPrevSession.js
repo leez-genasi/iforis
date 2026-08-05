613ff5083d04b1a94bc0c2d6c7287cd5720294ba
leez-genasi <charlee3279@gmail.com>
2026-08-05 20:55:30 +0800

    fixed getPrevSession.js

--- content/templater/scripts/getPrevSession.js
+++ content/templater/scripts/getPrevSession.js
@@ -1,6 +1,6 @@
 function getPrevSession() {
     let files = app.vault.getMarkdownFiles()
-        .filter(f => f.path.startsWith("sessions/"))
+        .filter(f => f.path.startsWith("a_sessions/"))
         .sort((a, b) => a.stat.ctime - b.stat.ctime);
     let prevSession = files[files.length - 2];
 
